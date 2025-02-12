6bf29b8ec2ec795ebacc8bac70ea8cc3867c60d1
deepblue <deepblue@inha.ac.kr>
2025-02-12 16:35:08 +0900

    v2.2 day03 assignment

--- day03.py
+++ day03.py
@@ -1,33 +1,26 @@
-# Assignment Day 02
-# v1.4) Make my_pow custom function instead of ** operator, power function and make it work.
-import math
+# Assignment
+# v2.3) 다음 코드에서 딕셔너리를 제거하고 리스트만 사용하여 동일하게 동작하도록 코드를 수정하시오.
+import random
+drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
+japan_drinks_foods = {"사케": "광어회", "위스키": "낙곱새"}
+drinks_foods.update(japan_drinks_foods)
+drinks_foods_keys = list(drinks_foods)
 
-
-def my_pow(b, e) -> float:
-    """
-    A user-defined function that receives a base and exponent and returns the power result in the form of a real number
-    :param b: base number
-    :param e: exponent
-    :return: the power result in the form of a real number
-    """
-    if e < 0:  # minus value
-        b = 1 / b
-        e = e * -1
-
-    result = 1
-
-    # partitioning
-    i = int(e) 
-    f = e - i
-
-    for _ in range(i):  # for k in range(e):
-        result = result * b
-
-    # float value process
-    if f > 0:
-        result = result * math.exp(f * math.log(b))
-
-    return result
-
-
-# custom power function
+while True:
+    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) {drinks_foods_keys[4]}   6) 아무거나   7) 종료 : ')
+    if menu == '1':
+        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
+    elif menu == '2':
+        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
+    elif menu == '3':
+        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
+    elif menu == '4':
+        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
+    elif menu == '5':
+        print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
+    elif menu == '6':
+        random_drink = random.choice(drinks_foods_keys)
+        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
+    elif menu == '7':
+        print(f'다음에 또 오세요')
+        break
